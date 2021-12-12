69f79fe52cf0128cfdba2707a3374313cba14f08
fthomasvp <fellipecab@gmail.com>
2021-12-12 00:17:32 -0300

    chore: add MaterialApp and use Scaffold with a NetworkImage

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+            title: const Text('I Am Poor'), backgroundColor: Colors.brown[900]),
+        body: const Image(
+          image: NetworkImage(
+              'https://images.unsplash.com/photo-1569982175971-d92b01cf8694?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80'),
+        ),
+        backgroundColor: Colors.brown[700],
+      ),
+    );
+  }
+}
