6cba007b091e61175f0bf3b0927e8156ab79912f
fthomasvp <fellipecab@gmail.com>
2021-12-12 22:47:08 -0300

    feat: add Column widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,32 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
             title: const Text('I Am Poor'), backgroundColor: Colors.brown[900]),
         body: SafeArea(
-          child: Container(
-            color: Colors.blueGrey[300],
-            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-            padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
-            child: const Text('Container com Margem e Padding'),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.blueGrey[300],
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+                child: const Text('Container com Margem e Padding 1'),
+              ),
+              Container(
+                color: Colors.blueGrey[300],
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+                child: const Text('Container com Margem e Padding 2'),
+              ),
+              Container(
+                color: Colors.blueGrey[300],
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+                child: const Text('Container com Margem e Padding 3'),
+              ),
+            ],
           ),
         ),
         backgroundColor: Colors.brown[700],
