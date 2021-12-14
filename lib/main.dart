f17064af02f54ad0663c335844b9d9a53102ea97
fthomasvp <fellipecab@gmail.com>
2021-12-14 00:17:16 -0300

    feat: add custom fonts

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,13 @@ class MyApp extends StatelessWidget {
                 margin:
                     const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
                 padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
-                child: const Text('Container com Margem e Padding 1'),
+                child: const Text(
+                  'Container com Margem e Padding 1',
+                  style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w600),
+                ),
               ),
               Container(
                 color: Colors.blueGrey[300],
